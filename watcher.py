bd84068511db7936ba18d75a502e84405f224754
cuongleminh123 <thanhdantran324@gmail.com>
2025-07-30 13:21:21 +0700

    Update watcher.py

--- watcher.py
+++ watcher.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7262217536:AAHX6ROfNRk1ZH9ZcwokcwvyR4jFtk3fKP8'
-ADMIN_ID = '1065510487'
+API_TOKEN = '8385872959:AAGJbMkOejYsveH4ZOtab3gbFV0lgMmEflI'
+ADMIN_ID = '6454123620'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
